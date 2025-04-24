edd91f5877a16009cf05bca9f000169f9266ff27
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    zeus: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Powershare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
